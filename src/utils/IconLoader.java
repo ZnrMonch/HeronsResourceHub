c512ec2c628762a04980464a0ca8e530a2760b5c
ZnrMonch <znermoncinilla@gmail.com>
2026-05-01 02:41:51 +0800

    updated utils

--- src/utils/IconLoader.java
+++ src/utils/IconLoader.java
@@ -1,6 +1,6 @@
 package utils;
 
-import java.awt.Image;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
@@ -34,12 +34,50 @@ public class IconLoader {
 	public static ImageIcon loadAndScaleIcon(String path, int width, int height) {
 		try {
 			BufferedImage img = ImageIO.read(IconLoader.class.getResource(path));
-			Image scaledImg = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+			Image scaledImg = getHighQualityScaledImage(img, width, height);
 			return new ImageIcon(scaledImg);
 		} catch (Exception e) {
 			System.err.println("Couldn't find file: " + path);
 			e.printStackTrace();
 			return null;
 		}
+	}
+	
+	public static ImageIcon loadAndScaleColorizedIcon(String path, int width, int height, Color color) {
+        try {
+            BufferedImage img = ImageIO.read(IconLoader.class.getResource(path));
+            
+            // Create a new image with transparency support
+            BufferedImage colorizedImg = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = colorizedImg.createGraphics();
+            
+            // Draw the original image first
+            g2d.drawImage(img, 0, 0, null);
+            
+            // Set the composite to SrcAtop (replaces existing pixels with the new color, ignores transparent areas)
+            g2d.setComposite(AlphaComposite.SrcAtop);
+            g2d.setColor(color);
+            g2d.fillRect(0, 0, img.getWidth(), img.getHeight());
+            g2d.dispose();
+
+            // Scale the newly colorized image
+            Image scaledImg = getHighQualityScaledImage(colorizedImg, width, height);
+            return new ImageIcon(scaledImg);
+        } catch (Exception e) {
+            System.err.println("Couldn't find or colorize file: " + path);
+            e.printStackTrace();
+            return null;
+        }
+	}
+	
+	private static Image getHighQualityScaledImage(BufferedImage src, int width, int height) {
+	    BufferedImage scaled = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2d = scaled.createGraphics();
+	    g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+	    g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+	    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	    g2d.drawImage(src, 0, 0, width, height, null);
+	    g2d.dispose();
+	    return scaled;
 	}
 }
