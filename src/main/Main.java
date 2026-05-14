556f0dc485968d3c4ef9ce0cc7da042b88446c89
cathr <cathr@ASUS>
2026-05-14 18:26:46 +0800

    sa log in and registration may changes ulit yan later

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,8 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		FontLib.loadFonts();
 		
-//		new Auth();
-//		new Page(new AdminPanel());
-		new Page(new Marketplace());
+	      new Auth();
+		  //new Page(new Profile()); 
 	}
 }
