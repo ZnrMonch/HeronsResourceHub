a7894d8a888eb2a5d9273d9cd87b41c258f8b88b
ZnrMonch <znermoncinilla@gmail.com>
2026-05-16 17:13:42 +0800

    added user record for the database

---
+++ src/database/UserRecord.java
@@ -0,0 +1,21 @@
+package database;
+
+public class UserRecord {
+	public String systemRole;
+	public String studentId;
+	public String umakEmailAddress;
+	public String password;
+	public String college;
+	public String yearLevel;
+	public String courseProgram;
+	public String firstName;
+	public String lastName;
+	public int karmaScore;
+	public String profileImage;
+	public Long contactNum;
+	public String homeAddress;
+	public Long gcashNum;
+	public Long mayaNum;
+	public String mastercardNum;
+	public String visaNum;
+}
